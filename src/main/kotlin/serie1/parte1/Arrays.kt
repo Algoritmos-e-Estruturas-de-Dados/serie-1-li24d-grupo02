4e34feb558ae955d0dba0ca6f6e58710fe0f7221
Vasco Batista <82042986+VascoRB@users.noreply.github.com>
2025-03-17 12:31:59 +0000

    Find Min Dif

--- src/main/kotlin/serie1/parte1/Arrays.kt
+++ src/main/kotlin/serie1/parte1/Arrays.kt
@@ -1,7 +1,18 @@
 package parte1
 
+import kotlin.math.absoluteValue
+
 fun findMinDifference(elem1: IntArray, elem2: IntArray): Int {
-    throw UnsupportedOperationException()
+    if (elem1.isEmpty() or elem2.isEmpty()) return -1
+    var i = 0
+    var j = 0
+    var diff: Int = (elem1[0] - elem2[0]).absoluteValue
+    while (i < elem1.size && j < elem2.size) {
+        if(diff > (elem1[i] - elem2[j]).absoluteValue) diff = (elem1[i] - elem2[j]).absoluteValue
+        if (elem1[i] < elem2[j]) i++
+        else j++
+    }
+    return diff
 }
 
 fun counter(array: IntArray, k: Int, lower: Int, upper: Int): Pair<Int, Int> {
