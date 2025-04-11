51d0a7ac6b82117f6e88ddb39aa8de7805101cf9
RogueCoder <44646201+lolowlowk@users.noreply.github.com>
2025-04-11 02:07:51 +0100

    Update ReadUtils.kt

--- src/main/kotlin/serie1/problema/ReadUtils.kt
+++ src/main/kotlin/serie1/problema/ReadUtils.kt
@@ -13,8 +13,27 @@ fun createWriter(fileName: String): PrintWriter {
     return PrintWriter(fileName)
 }
 
+/**
+ * Reads a chunk of integers from a [BufferedReader] and returns them as an [IntArray].
+ *
+ * This function reads [chunkSize] lines from the [BufferedReader], converts each line to an integer,
+ * and returns an array of those integers.
+ *
+ * @receiver The [BufferedReader] from which the integers will be read.
+ * @param chunkSize The number of integers (lines) to read.
+ * @return An [IntArray] containing the integers read from the file.
+ */
+fun BufferedReader.readIntChunk(chunkSize: Int): IntArray {
+    val numbers = IntArray(chunkSize)
+    for (i in 0 until chunkSize) {
+        val line = kotlin.io.readLine()
+        numbers[i] = line!!.toInt()
+    }
+    return numbers
+}
+
 /** Usage Example
- *  Files "input.txt" and "ouput.txt" are on the project Directory.
+ *  Files "input.txt" and "output.txt" are on the project Directory.
  *  This program copies the Input File to OutputFile.
  */
 fun main() {
@@ -30,21 +49,3 @@ fun main() {
     br.close()
     pw.close()
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
